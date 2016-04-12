339ac0721f1636cc0e819029e3243f14baea0d5e
Javier Casas <javcasas@gmail.com>
2016-04-12 12:47:15 -0500

    Lots of operations

--- src/Data/Decimal.elm
+++ src/Data/Decimal.elm
@@ -3,10 +3,29 @@ module Data.Decimal
     , fromInt
     , fromIntWithExponent
     , fromString
+    , unsafeFromString
+    , toString
+    , toFloat
+    , fromFloat
     , add
     , sub
     , negate
     , mul
+    , fastdiv
+    , abs
+    , gt
+    , gte
+    , eq
+    , neq
+    , lt
+    , lte
+    , compare
+    , zero
+    , one
+    , minusOne
+    , truncate
+    , round
+    , getDigit
     ) where
 
 
@@ -19,6 +38,7 @@ module Data.Decimal
 import Data.Integer
 import Maybe exposing (Maybe)
 import String
+import Debug
 
 
 type alias Mantissa = Data.Integer.Integer
@@ -41,9 +61,15 @@ fromIntWithExponent n e =
 fromString : String -> Maybe Decimal
 fromString s =
     let stringToDecimal s =
-        case Data.Integer.fromString s of
-            Nothing -> Nothing
-            Just(a) -> Just (Decimal a 0)
+        let stringIntToDecimal s' e =
+            case Data.Integer.fromString s' of
+                Nothing -> Nothing
+                Just(a) -> Just (Decimal a e)
+        in
+        case String.split "." s of
+            [a, b] -> stringIntToDecimal (a ++ b) (-(String.length b))
+            [a] -> stringIntToDecimal a 0
+            _ -> Nothing
     in
     let makeMantissa s =
         case String.split "." s of
@@ -58,9 +84,56 @@ fromString s =
             _ -> (Nothing, Err "")
     in
     case splitMantissaExponent s of
-        (Just (Decimal m a), Ok e) -> Just (Decimal m e)
+        (Just (Decimal m a), Ok e) -> Just (Decimal m (e+a))
         _ -> Nothing
-    
+
+unsafeFromString : String -> Decimal
+unsafeFromString s =
+    case fromString s of
+        Just a -> a
+        Nothing -> Debug.crash "Invalid string for Decimal"
+
+insert_decimal_period : Int -> String -> String
+insert_decimal_period pos s =
+    let extra_zeros = pos - String.length s
+        padded_s = if extra_zeros >= 0 then (String.repeat (extra_zeros+1) "0") ++ s else s
+        before = String.dropRight pos padded_s
+        after = String.right pos padded_s
+    in
+    before ++ "." ++ after
+
+toString : Decimal -> String
+toString (Decimal m e) =
+    let abs_m = if m `Data.Integer.gte` (Data.Integer.fromInt 0) then m else Data.Integer.opposite m
+        s = Data.Integer.toString abs_m
+        sign = if m `Data.Integer.gte` (Data.Integer.fromInt 0) then "" else "-"
+        add_zeros n = String.repeat n "0"
+    in    
+    case Basics.compare e 0 of
+        EQ -> sign ++ s
+        GT -> sign ++ s ++ (add_zeros e)
+        LT -> sign ++ insert_decimal_period (0-e) s
+
+toFloat : Decimal -> Float
+toFloat d = 
+    case String.toFloat (toString d) of
+        Ok a -> a
+        Err _ -> 42.0
+
+fromFloat : Float -> Decimal
+fromFloat f =
+    case fromString (Basics.toString f) of
+        Just a -> a
+        Nothing -> fromInt 42
+
+fastdiv : Decimal -> Decimal -> Decimal
+fastdiv a b =
+    let fa = toFloat a
+        fb = toFloat b
+        res = fa / fb
+    in
+    fromFloat res 
+
 {-roundToExponent : Exponent -> Decimal -> Decimal-}
 
 addDecimals : Int -> Decimal -> Decimal
@@ -99,3 +172,121 @@ sub a b = add a (negate b)
 mul : Decimal -> Decimal -> Decimal
 mul (Decimal ma ea) (Decimal mb eb) =
     Decimal (ma `Data.Integer.mul` mb) (ea + eb)
+
+abs : Decimal -> Decimal
+abs (Decimal m e) =
+    case Data.Integer.compare m (Data.Integer.fromInt 0) of
+        LT -> Decimal (Data.Integer.opposite m) e
+        _ -> Decimal m e
+
+compare : Decimal -> Decimal -> Order
+compare a b =
+    let (fa, fb) = toCommonExponent (a, b)
+        (Decimal ma ea) = fa
+        (Decimal mb eb) = fb
+    in
+    Data.Integer.compare ma mb
+
+eq : Decimal -> Decimal -> Bool
+eq a b =
+    case compare a b of
+        EQ -> True
+        _ -> False
+
+neq : Decimal -> Decimal -> Bool
+neq a b = not (eq a b)
+
+gt : Decimal -> Decimal -> Bool
+gt a b =
+    case compare a b of
+        GT -> True
+        _ -> False
+
+gte : Decimal -> Decimal -> Bool
+gte a b = (gt a b) || (eq a b)
+
+lt : Decimal -> Decimal -> Bool
+lt a b =
+    case compare a b of
+        LT -> True
+        _ -> False
+
+lte : Decimal -> Decimal -> Bool
+lte a b = (lt a b) || (eq a b)
+
+zero : Decimal
+zero = fromInt 0
+
+one : Decimal
+one = fromInt 1
+
+minusOne : Decimal
+minusOne = fromInt -1
+
+isPositive : Decimal -> Bool
+isPositive x = x `gte` zero
+
+isNegative : Decimal -> Bool
+isNegative x = not (isPositive x)
+
+getDigit : Int -> Decimal -> Int
+getDigit n d =
+    let s = toString d in
+    let toInt d =
+        case d of
+            "1" -> 1
+            "2" -> 2
+            "3" -> 3
+            "4" -> 4
+            "5" -> 5
+            "6" -> 6
+            "7" -> 7
+            "8" -> 8
+            "9" -> 9
+            "0" -> 0
+            "" -> 0
+            _ -> -1
+    in
+    case (String.split "." s, Basics.compare n 0) of
+        ([a], GT) ->
+            toInt (String.right 1 (String.dropRight n a))
+        ([a], EQ) ->
+            toInt (String.right 1 a)
+        ([a], LT) ->
+            0
+        ([a, b], GT) ->
+            toInt (String.right 1 (String.dropRight n a))
+        ([a, b], EQ) ->
+            toInt (String.right 1 a)
+        ([a, b], LT) ->
+            toInt (String.left 1 (String.dropLeft (-n-1) b))
+        _ -> -13
+
+truncate : Int -> Decimal -> Decimal
+truncate n d =
+    let s = toString d in
+    let toDecimal s =
+        case fromString s of
+            Just a -> a
+            Nothing -> zero
+    in
+    case (String.split "." s, n >= 0) of
+        ([a], True) -> toDecimal (String.dropRight n a ++ String.repeat n "0")
+        ([a], False) -> toDecimal a
+        ([a, b], True) -> toDecimal (String.dropRight n a ++ String.repeat n "0")
+        ([a, b], False) -> toDecimal (a ++ "." ++ String.left (-n)  b)
+        _ -> zero
+
+signAsInt : Decimal -> Int
+signAsInt d =
+    case compare d zero of
+        LT -> -1
+        EQ -> 0
+        GT -> 1
+
+round : Int -> Decimal -> Decimal
+round n d =
+    let t = truncate n d
+        next_digit = getDigit (n - 1) d
+        to_increment = fromIntWithExponent (signAsInt d) n in
+    if next_digit >= 5 then t `add` to_increment else t
--- test/Test.elm
+++ test/Test.elm
@@ -3,6 +3,18 @@ import Maybe exposing (Maybe)
 import ElmTest exposing (..)
 import Graphics.Element exposing (show, flow, down, Element, leftAligned)
 
+liftMaybe : (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
+liftMaybe f a b =
+    case (a, b) of
+        (Just va, Just vb) -> Just (f va vb)
+        _ -> Nothing
+dtoString = Data.Decimal.toString
+dnegate = Data.Decimal.negate
+dcompare : Maybe Decimal -> Maybe Decimal -> Maybe Order
+dcompare = liftMaybe Data.Decimal.compare
+dtruncate = Data.Decimal.truncate
+dround = Data.Decimal.round
+uFromString = Data.Decimal.unsafeFromString
 
 addTests : Test
 addTests =
@@ -22,7 +34,7 @@ subTests =
     in
     suite "sub testsuite"
         [ equals (three `sub` two) one
-        , equals (Data.Decimal.negate (Data.Decimal.negate one)) one
+        , equals (dnegate (dnegate one)) one
         ]
 
 mulTests : Test
@@ -34,7 +46,7 @@ mulTests =
     in
     suite "mul testsuite"
         [ equals (mul two three) six
-        , equals (mul two (Data.Decimal.negate three)) (Data.Decimal.negate six)
+        , equals (mul two (dnegate three)) (dnegate six)
         ]
 
 fromStringTests : Test
@@ -46,6 +58,70 @@ fromStringTests =
         , equals (fromString "sdfwe") (Nothing)
         , equals (fromString "300e0") (Just (fromInt 300))
         , equals (fromString "300E2") (Just (fromIntWithExponent 300 2))
+        , equals (fromString "3.3") (Just (fromIntWithExponent 33 -1))
+        ]
+
+fromFloatTests : Test
+fromFloatTests =
+    suite "fromFloat testsuite"
+        [ equals (fromFloat 1) (fromInt 1)
+        , equals (fromFloat -1) (fromInt -1)
+        , equals (fromFloat -4521) (fromInt -4521)
+        , equals (fromFloat 3.3) (uFromString "3.3")
+        , equals (fromFloat -3.4) (uFromString "-3.4")
+        , equals (fromFloat 11e-1) (uFromString "1.1")
+        ]
+
+toStringTests : Test
+toStringTests =
+    suite "toString testsuite"
+        [ equals (dtoString (fromInt 1)) "1"
+        , equals (dtoString (fromInt -1)) "-1"
+        , equals (dtoString (fromIntWithExponent -1 1)) "-10"
+        , equals "0.1" (dtoString (fromIntWithExponent 1 -1)) 
+        , equals "-0.1" (dtoString (fromIntWithExponent -1 -1))
+        , equals "-1.1" (dtoString (fromIntWithExponent -11 -1))
+        , equals "-1234.5678" (dtoString (fromIntWithExponent -12345678 -4))
+        ]
+
+fastdivTests : Test
+fastdivTests =
+    suite "fastdiv testsuite"
+        [ equals "0.1" (dtoString ((fromInt 1) `fastdiv` (fromInt 10))) 
+        , equals "0.3333333333333333" (dtoString ((fromInt 1) `fastdiv` (fromInt 3))) 
+        , equals "0.6666666666666666" (dtoString ((fromInt 1200) `fastdiv` (fromInt 1800))) 
+        ]
+
+compareTests : Test
+compareTests =
+    suite "compare testsuite"
+        [ equals (Just EQ) (dcompare (fromString "1") (Just (fromInt 1)))
+        , equals (Just GT) (dcompare (fromString "10") (Just (fromInt 1)))
+        , equals (Just LT) (dcompare (fromString "1") (Just (fromInt 10)))
+        , equals (Just EQ) (dcompare (fromString "1.0") (Just (fromInt 1)))
+        , equals (Just GT) (dcompare (fromString "1.0") (Just (fromInt -10)))
+        ]
+
+roundTests : Test
+roundTests =
+    suite "round testsuite"
+        [ equals "0.3333" (dtoString (dround -4 ((fromInt 1) `fastdiv` (fromInt 3))))
+        , equals "0.6667" (dtoString (dround -4 ((fromInt 1200) `fastdiv` (fromInt 1800))))
+        , equals "-0.6667" (dtoString (dround -4 ((fromInt -1200) `fastdiv` (fromInt 1800))))
+        , equals 3 (getDigit 2 (uFromString "12345"))
+        , equals 0 (getDigit -1 (uFromString "12345"))
+        , equals 3 (getDigit 0 (uFromString "123.45"))
+        , equals 2 (getDigit 1 (uFromString "123.45"))
+        , equals 4 (getDigit -1 (uFromString "123.45"))
+        , equals 0 (getDigit 2 (uFromString "0.45"))
+        ]
+
+truncateTests : Test
+truncateTests =
+    suite "truncate testsuite"
+        [ equals "0.3333" (dtoString (dtruncate -4 ((fromInt 1) `fastdiv` (fromInt 3))))
+        , equals "0.6666" (dtoString (dtruncate -4 ((fromInt 1200) `fastdiv` (fromInt 1800))))
+        , equals "-0.6666" (dtoString (dtruncate -4 ((fromInt -1200) `fastdiv` (fromInt 1800))))
         ]
 
 allTests : Test
@@ -55,6 +131,12 @@ allTests =
         , subTests
         , mulTests
         , fromStringTests
+        , fromFloatTests
+        , toStringTests
+        , fastdivTests
+        , compareTests
+        , roundTests
+        , truncateTests
         ]
 
 main : Element
